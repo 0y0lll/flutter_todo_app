3b69cbe27a83686a45d06b7bfbd354990b842e75
Leona3 <leona@musma.net>
2024-01-24 10:41:54 +0900

    feat: 암튼 지간 뭔가 하긴 했는...

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 101, 158, 114)),
-        useMaterial3: true,
-      ),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 101, 158, 114)),
+          useMaterial3: true,
+          textTheme: const TextTheme(
+            bodyMedium: TextStyle(letterSpacing: -1.2),
+          )),
       home: const MyHomePage(title: 'Awesome todo'),
     );
   }
@@ -45,6 +47,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   double bottom = -200.0; // 초기값으로 밑에서 위로 올린 상태
 
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+
   void togglePositionedVisibility() {
     setState(() {
       if (bottom < 0) {
@@ -58,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 101, 158, 114),
+      backgroundColor: const Color.fromARGB(255, 101, 158, 114),
       // appBar: AppBar(
       //   backgroundColor: Colors.transparent,
       //   leading: IconButton(onPressed: () {}, icon: Icon(Icons.home)),
@@ -100,35 +105,161 @@ class _MyHomePageState extends State<MyHomePage> {
                     itemBuilder: (BuildContext context, int index) {
                       Todo item = newDocs[index];
 
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 10),
-                        child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(8),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color.fromARGB(255, 46, 87, 55)
-                                    .withOpacity(0.5),
-                                spreadRadius: 1,
-                                blurRadius: 10,
-                                offset: const Offset(0, 8),
-                              )
-                            ],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 15, vertical: 10),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(item.title),
-                                Text(item.content),
-                                Text(item.updateDate),
+                      TextEditingController titleController =
+                          TextEditingController(text: item.title);
+                      TextEditingController contentController =
+                          TextEditingController(text: item.content);
+
+                      return GestureDetector(
+                        onTap: () {
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (context) {
+                              return Container(
+                                width: double.infinity,
+                                decoration: const BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(16),
+                                      topRight: Radius.circular(16),
+                                    )),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 40, vertical: 20),
+                                  child: Column(
+                                    children: [
+                                      TextField(
+                                        controller: titleController,
+                                        decoration: const InputDecoration(
+                                            hintText: 'What your Todo?'),
+                                      ),
+                                      const SizedBox(height: 10),
+                                      TextField(
+                                        minLines: 14,
+                                        maxLines: 14,
+                                        controller: contentController,
+                                        keyboardType: TextInputType.multiline,
+                                        decoration:
+                                            const InputDecoration.collapsed(
+                                                hintText:
+                                                    'Enter your Todo here'),
+                                      ),
+                                      Expanded(
+                                          child: SizedBox(
+                                        width: double.infinity,
+                                        child: ElevatedButton(
+                                            onPressed: () async {
+                                              if (titleController
+                                                      .text.isEmpty ||
+                                                  contentController
+                                                      .text.isEmpty) {
+                                                return;
+                                              }
+
+                                              Todo data = Todo(
+                                                  title: titleController.text,
+                                                  content:
+                                                      contentController.text,
+                                                  date:
+                                                      DateTime.now().toString(),
+                                                  updateDate:
+                                                      DateTime.now().toString(),
+                                                  isPlanned: true,
+                                                  isDoing: false,
+                                                  isDone: false);
+
+                                              await FirebaseFirestore.instance
+                                                  .collection('todos')
+                                                  .add(data.toFirestore());
+                                            },
+                                            child: const Text('Update')),
+                                      )),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: const Color.fromARGB(255, 46, 87, 55)
+                                      .withOpacity(0.5),
+                                  spreadRadius: 1,
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 8),
+                                )
                               ],
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 15),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Text(item.title,
+                                          style: const TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold)),
+                                      GestureDetector(
+                                        onTap: () async {
+                                          final doc = await FirebaseFirestore
+                                              .instance
+                                              .collection('todos')
+                                              .where('title',
+                                                  isEqualTo: item.title)
+                                              .get()
+                                              .then((value) =>
+                                                  value.docs.map((e) => e.id));
+
+                                          Todo data = Todo(
+                                              title: item.title,
+                                              content: item.content,
+                                              date: item.date,
+                                              updateDate:
+                                                  DateTime.now().toString(),
+                                              isPlanned: !item.isPlanned,
+                                              isDoing: false,
+                                              isDone: !item.isDone);
+
+                                          await FirebaseFirestore.instance
+                                              .collection('todos')
+                                              .doc(doc.first)
+                                              .update(data.toFirestore());
+                                        },
+                                        child: Icon(
+                                            Icons.check_circle_outline_outlined,
+                                            size: 30,
+                                            color: item.isDone
+                                                ? Colors.green
+                                                : Colors.grey),
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(item.content),
+                                  const SizedBox(height: 10),
+                                  Text(item.updateDate,
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.grey.shade600)),
+                                ],
+                              ),
                             ),
                           ),
                         ),
@@ -162,7 +293,7 @@ class _MyHomePageState extends State<MyHomePage> {
             builder: (context) {
               return Container(
                 width: double.infinity,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(16),
@@ -174,9 +305,44 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Column(
                     children: [
                       TextField(
-                        decoration: InputDecoration(hintText: 'title hint'),
+                        controller: titleController,
+                        decoration:
+                            const InputDecoration(hintText: 'What your Todo?'),
                       ),
-                      TextField()
+                      const SizedBox(height: 10),
+                      TextField(
+                        minLines: 14,
+                        maxLines: 14,
+                        controller: contentController,
+                        keyboardType: TextInputType.multiline,
+                        decoration: const InputDecoration.collapsed(
+                            hintText: 'Enter your Todo here'),
+                      ),
+                      Expanded(
+                          child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              if (titleController.text.isEmpty ||
+                                  contentController.text.isEmpty) {
+                                return;
+                              }
+
+                              Todo data = Todo(
+                                  title: titleController.text,
+                                  content: contentController.text,
+                                  date: DateTime.now().toString(),
+                                  updateDate: DateTime.now().toString(),
+                                  isPlanned: true,
+                                  isDoing: false,
+                                  isDone: false);
+
+                              await FirebaseFirestore.instance
+                                  .collection('todos')
+                                  .add(data.toFirestore());
+                            },
+                            child: const Text('Add')),
+                      )),
                     ],
                   ),
                 ),
--- lib/todo_state.dart
+++ lib/todo_state.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
 
 class Todo {
+  String? id;
   String title;
   String content;
   String date;
@@ -11,6 +11,7 @@ class Todo {
   bool isDone;
 
   Todo({
+    this.id,
     required this.title,
     required this.content,
     required this.date,
@@ -20,17 +21,20 @@ class Todo {
     required this.isDone,
   });
 
+  @override
+  String toString() {
+    return 'Todo{id: $id, title: $title, content: $content, date: $date, updateDate: $updateDate, isPlanned: $isPlanned, isDoing: $isDoing, isDone: $isDone}';
+  }
+
   factory Todo.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot) {
     final data = snapshot.data();
 
     return Todo(
+        id: data?['id'],
         title: data?['title'],
         content: data?['content'],
-        date:
-            DateFormat.yMMMMd('ko_KR').add_jm().format(data?['date'].toDate()),
-        updateDate: DateFormat.yMMMMd('ko_KR')
-            .add_jm()
-            .format(data?['updateDate'].toDate()),
+        date: data != null ? data['date'].toString() : '',
+        updateDate: data != null ? data['updateDate'].toString() : '',
         isPlanned: data?['isPlanned'],
         isDoing: data?['isDoing'],
         isDone: data?['isDone']);
@@ -47,4 +51,12 @@ class Todo {
       'isDone': isDone,
     };
   }
+}
+
+class TodoStatus {
+  bool isPlanned = true;
+  bool isDoing = false;
+  bool isDone = false;
+
+  TodoStatus();
 }
